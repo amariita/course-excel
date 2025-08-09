e288c7657f6a48b79a41485ed551d2e63cff4b68
AmaRita <amariita@yandex.ru>
2025-08-09 15:09:05 +0500

    Finish project config

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+import { defineConfig } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: fixupConfigRules(compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+    )),
+
+    plugins: {
+        react: fixupPluginRules(react),
+        "react-hooks": fixupPluginRules(reactHooks),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        ecmaVersion: 2020,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "no-unused-vars": "warn",
+        indent: ["warn", 2],
+        semi: ['error', 'never'],
+        quotes: ["warn", "single"],
+        "no-console": "warn",
+        "no-multi-spaces": "error",
+        "comma-dangle": "off",
+        "require-jsdoc": "off"
+    },
+}]);
